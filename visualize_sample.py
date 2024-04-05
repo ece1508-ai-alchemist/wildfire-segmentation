2b2c83daf25122085de59b1cc57a22cd2ac9616f
Not-Forrest <forrestjiashuzhang@gmail.com>
2024-04-04 23:43:56 -0400

    Updating Visualize Sample, Adding Test Script, Adding new model pretrained chkpt

--- visualize_sample.py
+++ visualize_sample.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd())
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 import torch
 from torch import optim
 
@@ -18,13 +19,12 @@ from src.train.train import load_checkpoint
 def adjust_image(image):
     rgb_image = image[:, :, [2, 1, 0]]
     # Normalize the values to range between 0 and 1
-    # ABSOLUTELY BAD HACK
     if np.min(rgb_image) != 0:
         rgb_image = (rgb_image + -1 * np.min(rgb_image)) / (-2 * np.min(rgb_image))
-    # print(np.min(rgb_image))
-
+    #print(np.min(rgb_image))
+    
     rgb_image = rgb_image.astype(np.float32)
-    # print(rgb_image)
+    #print(rgb_image)
     if np.max(rgb_image) != 0:
         rgb_image /= np.max(rgb_image)
 
@@ -33,7 +33,6 @@ def adjust_image(image):
     rgb_image = np.clip(rgb_image ** (1 / gamma), 0, 1)  # Apply gamma correction
 
     return rgb_image
-
 
 # This works with CPU only!
 def print_sample(batch, sample_id):
@@ -50,9 +49,8 @@ def print_sample(batch, sample_id):
     ax1 = fig.add_subplot(1, 3, 1)
     ax1.imshow(pre_fire_rgb)
 
-    ax1 = fig.add_subplot(1, 3, 2)
+    ax1 = fig.add_subplot(1,3,2)
     ax1.imshow(flip_mask)
-
 
 def print_sample_and_pred(batch, pred, sample_id):
     image = batch["image"]
@@ -80,23 +78,23 @@ def print_sample_and_pred(batch, pred, sample_id):
     ax1.imshow(flip_pred)
     ax1.set_title("Predicted Mask")
 
-
 if __name__ == "__main__":
     print("Adjust the loop below to print the sample you want")
 
     SAVE_PATH = "model_weights"
-    LOAD_MODEL = 1  # set to 1 if there are model weights ready to be loaded
-    CHECKPOINT_NAME = "pre_trained.pt"
+    LOAD_MODEL  = 1 # set to 1 if there are model weights ready to be loaded
+    CHECKPOINT_NAME = "pre_trained.chkpt" #Set this to name of the weights
     THRESHOLD = 0.5
-    NUM_SAMPLES = 8  # Number of samples to print
+    BATCH_SIZE = 8
+    BATCH_NUM = 0 # Which batch to print
 
+    #mean, std = get_mean_std(trial_loader)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = "cpu"
-
     print(device)
-
     dt = WildfireDataset(POST_FIRE_DIR, transforms=None)
 
-    val_loader = get_loader(dt.train, is_train=False, loader_args=dict(batch_size=1, shuffle=True))
+    trial_loader = get_loader(dt.test, is_train=False, loader_args=dict(batch_size=BATCH_SIZE, shuffle=False))
 
     if LOAD_MODEL:
         model = UNet(n_channels=12, n_classes=1, bilinear=False)
@@ -106,26 +104,36 @@ if __name__ == "__main__":
         scaler = torch.cuda.amp.GradScaler()
 
         CUR_EPOCH = 0
-        # Load checkpoint
+        # Load checkpoint 
         CUR_EPOCH = load_checkpoint(os.path.join(SAVE_PATH, CHECKPOINT_NAME), model, scaler, optimizer)
 
+       
         model.eval()
 
-    num_samples_done = 0
-    for batch in val_loader:
+    #print(len(trial_loader))
+    num_batches_done = 0
+    for i, batch in enumerate(trial_loader):
+
+        if i != BATCH_NUM:
+            continue
+
         images, true_masks = batch["image"], batch["mask"]
 
         images = images.to(device)
         true_masks = true_masks.to(device)
-
         if LOAD_MODEL:
             with torch.cuda.amp.autocast():
                 masks_pred = model(images)
             # Calculate metrics
             preds_binary = (torch.sigmoid(masks_pred) > THRESHOLD).float()
-            print_sample_and_pred(batch, preds_binary, 0)
+            for j in range(BATCH_SIZE):
+                print_sample_and_pred(batch, preds_binary, j)
         else:
-            print_sample(batch, 0)
-        num_samples_done += 1
-        if num_samples_done == NUM_SAMPLES:
-            break
+            for j in range(BATCH_SIZE):
+                print_sample(batch, j)
+            
+        break
+
+
+
+ 
